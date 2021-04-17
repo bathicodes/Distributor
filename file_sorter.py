b4d09c0f7457c37cadbe756f321fe70b4cfbd356
Bathiya Seneviratne <Seneviratne.bathiya@gmail.com>
2021-04-17 12:23:41 +0530

    File sorter is working

--- file_sorter.py
+++ file_sorter.py
@@ -7,13 +7,13 @@ def sorter(path):
     dirpath = Path(path)
     for files in dirpath.iterdir():
         if files.suffix in [".rtfd",".rtf",".pdf",".doc",".docx",".pdf",".odt",".txt",".tex",".wpd"]:
-            print("doc file")
+            os.replace(files,f"/Users/bathiyaseneviratne/Documents/{files.name}")
         elif files.suffix in [".mp3",".aif",".cda",".mid",".midi",".mpa",".ogg",".wav",".wma",".wpi"]:
-            print("music")
+            os.replace(files,f"/Users/bathiyaseneviratne/Music/{files.name}")
         elif files.suffix in [".png",".jpg",".jpeg",".tif", ".tiff",".bmp",".gif",".eps"]:
-            print("Image")
+            os.replace(files,f"/Users/bathiyaseneviratne/Pictures/{files.name}")
         elif files.suffix in [".3g2",".3gp",".avi",".flv",".h264",".m4v",".mkv",".mov",".mp4",".mpg",".mpeg",".rm",".swf",".vob",".wmv"]:
-            print("video")
+            os.replace(files,f"/Users/bathiyaseneviratne/Movies/{files.name}")
        
 
     
--- folder_checker.py
+++ folder_checker.py
@@ -5,8 +5,6 @@ path = r'/Users/bathiyaseneviratne/Desktop/Distributor'
 
 def check():
     if len(os.listdir(path) ) == 0:
-        print("Directory is empty") # remove later
         pass
     else:    
-        print("Directory is not empty")
         sorter(path)
