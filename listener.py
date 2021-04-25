3de3c6e0e83de539935ba44c778ef063d38dd5d0
Bathiya Seneviratne <Seneviratne.bathiya@gmail.com>
2021-04-25 11:14:43 +0530

    Create listener.py
    Automated the file distribution code.

---
+++ listener.py
@@ -0,0 +1,28 @@
+import sys
+import time
+import logging
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+from environment import *
+from main import main
+
+
+def on_created(event):
+    main()
+
+if __name__ == "__main__":
+
+    event_hander = FileSystemEventHandler()
+    event_hander.on_created = on_created
+    path = path_osx()
+    observer = Observer()
+    observer.schedule(event_hander, path, recursive=True)
+    observer.start()
+    try:
+        print("Monitoring...")
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+        print("Done monitoring!")
+    observer.join()
