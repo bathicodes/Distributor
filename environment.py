ed8e0c77889d64eb65c3d9b8d98842d41d123c4c
Bathiya Seneviratne <Seneviratne.bathiya@gmail.com>
2021-04-25 08:50:01 +0530

    Update environment.py

--- environment.py
+++ environment.py
@@ -1,21 +1,19 @@
 from pathlib import Path
 
-
-
 # -------------------- path_osx for mac os -------------------- #
-home = str(Path.home())
+home_osx = str(Path.home())
 
 def path_osx():
-    return f"{home}/Desktop/Distributor"
+    return f"{home_osx}/Desktop/Distributor"
      
 def documents_osx():
-    return f"{home}/Documents/"
+    return f"{home_osx}/Documents/"
 
 def music_osx():
-    return f"{home}/Music/"
+    return f"{home_osx}/Music/"
 
 def pictures_osx():
-    return f"{home}/Pictures/"
+    return f"{home_osx}/Pictures/"
 
 def movies_osx():
-    return f"{home}/Movies/"
+    return f"{home_osx}/Movies/"
